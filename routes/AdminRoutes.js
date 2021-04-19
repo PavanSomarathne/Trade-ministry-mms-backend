d88ac06f04c1e8542eea43af0c566870351f1c86
Pavan <pavanpabs@gmail.com>
2021-04-19 21:08:39 +0530

    fatch ml server

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Question = require("../schemas/Question");
 const auth = require("../Authentication/Auth");
+const fetch = require("node-fetch");
 
 router.post("/new-question", function (req, res, next) {
   Question.create({
@@ -36,6 +37,24 @@ router.delete("/questions", function (req, res, next) {
   Question.findByIdAndRemove({ _id: req.body.id }).then(function (item) {
     res.send(item);
   });
+});
+
+router.post("/developing-area", function (req, res, next) {
+  let obj = {
+    text: req.body.text,
+  };
+
+  fetch("https://mms-ml.herokuapp.com/api/", {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then(function (result) {
+      res.send(result);
+      //or possibly res.send(result), depending on what indeed responds with
+    })
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
