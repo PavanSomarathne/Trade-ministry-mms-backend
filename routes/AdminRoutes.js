a14b282106384184e18fd75ec3e76dd56f1966d8
Pavan <pavanpabs@gmail.com>
2021-06-05 02:03:12 +0530

    db connection to minutes

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const Question = require("../schemas/Question");
+const Minute = require("../schemas/Minute");
 const auth = require("../Authentication/Auth");
 
 router.post("/new-question", function (req, res, next) {
@@ -51,5 +52,31 @@ router.post("/developing-area", function (req, res, next) {
       console.error(error);
     });
 });
-
+router.post("/new-minute", function (req, res, next) {
+  Minute.create({
+    body: req.body.question,
+    meeting_name: req.body.name,
+    meeting_date: req.body.date,
+    meeting_time: req.body.time,
+    meeting_venue: req.body.venue,
+    present_private: req.body.private,
+    present_public: req.body.public,
+    present_academic: req.body.academic,
+    present_association: req.body.association,
+    excused: req.body.excused,
+    absent: req.body.absent,
+    meeting_approval_from: req.body.approval,
+    meeting_motion: req.body.motion,
+    meeting_motionBy: req.body.motionBy,
+    meeting_proposedBy: req.body.proposedBy,
+    meeting_secondedBy: req.body.secondedBy,
+    meeting_objective: req.body.objective,
+    meeting_activities: req.body.activities,
+    meeting_remarks: req.body.remarks,
+  })
+    .then(function (item) {
+      res.send(item);
+    })
+    .catch(next);
+});
 module.exports = router;
---
+++ schemas/Minute.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.set("useFindAndModify", false);
+
+const minute = new Schema({
+  meeting_name: {
+    type: String,
+    required: true,
+  },
+  meeting_date: {
+    type: String,
+    required: true,
+  },
+  meeting_time: {
+    type: String,
+    required: true,
+  },
+  meeting_venue: {
+    type: String,
+    required: true,
+  },
+  present_private: [
+    {
+      type: String,
+    },
+  ],
+  present_public: [
+    {
+      type: String,
+    },
+  ],
+  present_academic: [
+    {
+      type: String,
+    },
+  ],
+  present_association: [
+    {
+      type: String,
+    },
+  ],
+  excused: [
+    {
+      type: String,
+    },
+  ],
+  absent: [
+    {
+      type: String,
+    },
+  ],
+  meeting_approval_from: {
+    type: String,
+    required: true,
+  },
+  meeting_motion: {
+    type: String,
+  },
+  meeting_motionBy: {
+    type: String,
+  },
+  meeting_proposedBy: {
+    type: String,
+  },
+  meeting_secondedBy: {
+    type: String,
+  },
+  meeting_objective: {
+    type: String,
+  },
+  meeting_activities: [
+    { activity: String, action: String, responsibility: String },
+  ],
+  meeting_remarks: {
+    type: String,
+  },
+});
+
+const Minute = mongoose.model("minutes", minute);
+module.exports = Minute;
